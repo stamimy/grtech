c055a14239316517ae75143c6d9e641269742536
Suton Tamimy <suton.tamimy@gmail.com>
2024-12-22 00:12:02 +0700

    (3) Use database migration seeds to create these users,
    sail php artisan db:seed --class=DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@grtech.com',
+            'password' => Hash::make('password')
+        ]);
+
+        User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@grtech.com',
+            'password' => Hash::make('password')
         ]);
     }
 }
